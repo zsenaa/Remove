b8a4d791b6ac80221e351ce0e5c67cc88c5c26b1
Sena <zhrsnkunt@gmail.com>
2023-04-06 19:05:16 +0300

    Remove
    delete a repeating letter or word from a string

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("LÜTFEN BİR METİN GİRİNİZ : ");
+            string inputString = Console.ReadLine();
+            string resultString = string.Empty;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (!resultString.Contains(inputString[i]))
+                {
+                    resultString += inputString[i];
+                }
+            }
+            Console.WriteLine(resultString);
+            Console.ReadKey();
+        }
+    }
+}
